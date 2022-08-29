f52464bf9babfe75f563c3e3e3bbfdf6a435b4fb
Kirill <kirilltcarev1999@gmail.com>
2022-08-29 17:14:26 +0300

    нахождение max методом из первго семинара

--- Example005/Program.cs
+++ Example005/Program.cs
@@ -1,11 +1,16 @@
-﻿Console.Write("введите имя пользователя");
-string username = Console.ReadLine();
-if(username.ToLower() == "миша")
-{
-    Console.WriteLine("уходи,Миша");
-}
-else
-{
-    Console.Write("привет");
-    Console.WriteLine(username);
-}
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if(a > max ) max = a;
+if(b > max ) max = b;
+if(c > max ) max = c;
+if(d > max ) max = d;
+if(e > max ) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
