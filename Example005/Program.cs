60e47250c2de4b47d0ad1855b7cf28b4f65f0fa3
Kirill <kirilltcarev1999@gmail.com>
2022-08-29 17:06:42 +0300

    добавили алгоритм с условием

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("введите имя пользователя");
+string username = Console.ReadLine();
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("уходи,Миша");
+}
+else
+{
+    Console.Write("привет");
+    Console.WriteLine(username);
+}
